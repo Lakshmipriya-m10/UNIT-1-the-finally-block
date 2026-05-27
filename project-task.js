be9597c3295320e8e13c6dae2e83debf81ef789c
lakshmi priya <lakshmipriyaark@gmail.com>
2026-05-27 16:53:31 -0500

    Error debug -practice 5 done

--- project-task.js
+++ project-task.js
@@ -45,7 +45,16 @@ Step 4: Test Your Solution
 function processFile(fileName, fileData) {
   try {
     // TODO: Add input validation here
-    
+    if(!fileName){
+      throw new Error("Warning:No file name");
+    }
+    if(typeof fileData !== "string"){
+      throw new Error("Warning:File must be a string ");
+    }
+if(fileData.trim() === "")
+{
+  throw new Error("Warning :File not Empty");
+}
     // TODO: Implement simulated file processing here
     console.log(`Processing file: ${fileName}`);
     console.log(`File content: ${fileData}`);
@@ -54,10 +63,17 @@ function processFile(fileName, fileData) {
     
   } catch (err) {
     // TODO: Implement error handling
-    console.error(err);
+    console.error(`Found Error: ${err.message}`);
   }
+ 
   // TODO: Implement a finally block to close resources
+finally
+  {
+    console.log("Error Resolved");
+  }
+
 }
+
 
 // ============================================
 // 🧪 Test Cases Below
